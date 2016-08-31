c303bc1195876e28076ab825cd48e8c674650344
pluginmanage <1129652484@qq.com>
2016-08-31 14:08:35 +0800

    Update study.py

--- study.py
+++ study.py
@@ -3,14 +3,12 @@ import math
 def quadratic(a,b,c):
     x1=(math.sqrt(b*b-4*a*c)-b)/2*a
     x2=(-b-math.sqrt(b*b-4*a*c))/2*a
-    return x1,x2
-
 a=float(input('a为：'))
-b=float(input('b为：'))
-c=float(input('c为：'))
-d=(b*b-4*a*c)
+b=float(input('b为: '))
+c=float(input('c: '))
+d=b*b-4*a*c
 if d>=0:
     r=quadratic(a,b,c)
     print(r)
-else:
-    print('There is no real root')
+    else:
+        print('error')
